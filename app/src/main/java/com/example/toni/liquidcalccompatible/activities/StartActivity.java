7d898abce5add44c78997e102cb36b748f443148
Schloni1995 <33430528+Schloni1995@users.noreply.github.com>
2018-06-02 17:28:22 +0200

    cleaned up code firebase integration

--- app/src/main/java/com/example/toni/liquidcalccompatible/activities/StartActivity.java
+++ app/src/main/java/com/example/toni/liquidcalccompatible/activities/StartActivity.java
@@ -10,11 +10,17 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.example.toni.liquidcalccompatible.R;
 import com.example.toni.liquidcalccompatible.fragments.CalcFragment;
 import com.example.toni.liquidcalccompatible.fragments.NoticeFragment;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class StartActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener
@@ -41,7 +47,40 @@ public class StartActivity extends AppCompatActivity
 
         MenuItem menuItem = navigationView.getMenu().findItem(R.id.nav_calc);
         onNavigationItemSelected(menuItem);
+
+        firebaseOperation();
     }
+
+    private void firebaseOperation()
+    {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+        Log.d("Firebase", "Value inserted");
+
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("Firebase", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error)
+            {
+                // Failed to read value
+                Log.w("Firebase", "Failed to read value.", error.toException());
+            }
+        });
+    }
+
 
     @Override
     public void onBackPressed()
--- app/src/main/java/com/example/toni/liquidcalccompatible/fragments/CalcFragment.java
+++ app/src/main/java/com/example/toni/liquidcalccompatible/fragments/CalcFragment.java
@@ -314,9 +314,6 @@ public class CalcFragment extends Fragment
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
